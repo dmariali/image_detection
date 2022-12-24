ad9ab4d585500eebd53cae2835f88c5a84604323
YaseenAli <yaseenali99@gmail.com>
2022-12-24 00:10:09 -0500

    Initial Commit of backend. CORS error still present when api called from front end, but endpoint working from  postman.

--- src/App.js
+++ src/App.js
@@ -5,40 +5,40 @@ const App = () => {
   const [annotatedImage, setAnnotatedImage] = useState(null);
 
   const detect = async (image) => {
-    const fetch_url = 'http://127.0.0.1:8000/objectdetection/'
+    const fetch_url = "http://127.0.0.1:8000/";
     const response = await fetch(fetch_url, {
-      method: 'POST',
+      method: "GET",
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        "Accept": "application/json",
+        "Content-Type": "application/json",
       },
-      body: `{
-        "url": URL.createObjectURL(image),
-        "name": image.name,
-        }`,
-      });
-  
-      response.json().then(data => {
-        console.log(data);
-      });
-  }
+      // body: `{
+      //   "url": URL.createObjectURL(image),
+      //   "name": image.name,
+      //   }`,
+    });
+
+    response.json().then((data) => {
+      console.log(data);
+    });
+  };
 
   return (
-    <div className='App'>
+    <div className="App">
       <h1>Upload image</h1>
       <input
         type="file"
         name="myImage"
         onChange={(event) => {
-          detect(event.target.files[0])
+          detect(event.target.files[0]);
         }}
       />
 
       {annotatedImage && (
         <div>
-        {/* <img alt="not found" width={"250px"} src={URL.createObjectURL(annotatedImage)} /> */}
-        <br />
-        <button onClick={()=>setAnnotatedImage(null)}>Remove</button>
+          {/* <img alt="not found" width={"250px"} src={URL.createObjectURL(annotatedImage)} /> */}
+          <br />
+          <button onClick={() => setAnnotatedImage(null)}>Remove</button>
         </div>
       )}
     </div>
@@ -46,5 +46,3 @@ const App = () => {
 };
 
 export default App;
-
-
